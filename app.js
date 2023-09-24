14c6a81f955902e2c535a22b7cbf5d35b3bc0097
peetbernard <inline21@gmail.com>
2023-09-24 17:39:40 +0200

    updating project, auth processes done

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 
 // connectDB
 const connectDB = require('./db/connect')
-
+const authenticateUser = require('./middleware/authentication')
 // routers
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
@@ -18,8 +18,8 @@ app.use(express.json())
 // extra packages
 
 // routes
-app.use('api/v1/auth', authRouter)
-app.use('api/v1/jobs', jobsRouter)
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -16,12 +18,28 @@ const UserSchema = new mongoose.Schema({
     ],
     unique: true,
   },
-  name: {
+  password: {
     type: String,
     required: [true, 'Please provide password'],
     minlength: 6,
-    maxlength: 12,
   },
 })
+
+UserSchema.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+  next()
+})
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id, name: this.name }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  })
+}
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password)
+  return isMatch
+}
 
 module.exports = mongoose.model('User', UserSchema)
